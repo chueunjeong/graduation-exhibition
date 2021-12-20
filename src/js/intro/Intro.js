f2bb5e1465da9b7164462d390eba46c774b1f81f
rsa6 <rsa6@naver.com>
2021-12-21 06:22:11 +0900

    main edit

--- src/js/intro/Intro.js
+++ src/js/intro/Intro.js
@@ -4,7 +4,7 @@ import "../../assets/sass/intro.scss";
 const Intro = () => {
   return (
     <div className="container">
-      <div className="row align-items-center d-flex justify-content-center intro">
+      <div className="row align-items-center d-flex justify-content-center intro hidden-m">
         <div className="col-5 title">
           <a className="intro-title-a" href="/main">
             <div className="row fade-in-box">
@@ -16,6 +16,16 @@ const Intro = () => {
               <div className="col-10 text-left intro-title-text show-hover">An e ... g※</div>
             </div>
           </a>
+        </div>
+      </div>
+
+      <div className="row align-items-center d-flex justify-content-center intro hidden-pc">
+        <div className="intro-ellipse" style={{ marginTop: 5 }}></div>
+        <div className="text-center intro-title-text hidden-hover" style={{ marginLeft: 20 }}>
+          안녕하세요?
+        </div>
+        <div className="text-left intro-title-text show-hover" style={{ marginLeft: 20 }}>
+          An e ... g※
         </div>
       </div>
     </div>
