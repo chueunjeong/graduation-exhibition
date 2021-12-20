cc7043b0330239c0b2391866b16ae69189ef6664
chueunjeong <miho7724@gmail.com>
2021-12-20 16:57:13 +0900

    [chu]update 211220

---
+++ src/js/main/ImageList2.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import Ellipse from "../../assets/images/ellipse.png";
+import { data } from "../../common/store";
+
+import "../../assets/sass/carosel.scss";
+
+const ImageList = () => {
+  const [artistInfo, setArtistInfo] = useState([]);
+
+  useEffect(() => {
+    init();
+    //console.log("확인!!!!" + artistInfoList);
+  }, []);
+
+  const init = () => {
+    const getData = data;
+
+    setArtistInfo(getData);
+  };
+
+  return (
+    <div className="container imglist-container">
+      <div className="row imglist-row">
+        {artistInfo.map((artist, index) => (
+          <div className="row text-center imglist-item">
+            <div className="col-12">
+              <img className="imglist-image" src={artist.titleImage} key={index} alt={artist.name} />
+            </div>
+            <div className="col-12">
+              <img className="imglist-ellipse" src={Ellipse} alt="ellipse" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageList;
